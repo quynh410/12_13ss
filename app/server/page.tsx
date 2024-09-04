5d3cfd79c020f62b69b0b0d64b189aedadaa4b0c
quynh410 <quynhhhhhh07@gmail.com>
2024-09-04 16:30:20 +0700

    add

--- app/server/page.tsx
+++ app/server/page.tsx
@@ -9,10 +9,12 @@ export default async function Page() {
   const users = await GetUser();
   return (
     <div>
-        <p>render dữ liênụ bên server</p>
-      {users.map((item: any) => {
-        return <li key={item.id}>{item.name}</li>;
-      })}
+      <p>render dữ liênụ bên server</p>
+      <ul>
+        {users.map((item: any) => {
+          return <li key={item.id}>{item.name}</li>;
+        })}
+      </ul>
     </div>
   );
 }
