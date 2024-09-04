52566b79402eb966b86876477ce423b487c8ddaf
quynh410 <quynhhhhhh07@gmail.com>
2024-09-04 16:23:04 +0700

    add

--- app/server/page.tsx
+++ app/server/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 // hàm đi lấy dữ liêyj bằng call API
-async function getUser() {
+async function GetUser() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
   return data;
 }
 export default async function page() {
-  const users = await getUser();
+  const users = await GetUser();
   return (
     <div>
         <p>render dữ liênụ bên server</p>
